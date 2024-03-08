8dc81c1d9687f9a47347936c6b4fd984122b00dc
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2024-03-08 11:43:59 +0100

    Nye sortimentfiler med format som inkluderer postId for bruk av grunndata-db. Inkluderer også en golang-app som genererer ny fra gammel

---
+++ generate-sortiment.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("%s <input-file>\n", os.Args[0])
+	}
+
+	infilename := os.Args[1]
+	infile, err := ioutil.ReadFile(infilename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	indata := map[string][]int{}
+	if err = json.Unmarshal(infile, &indata); err != nil {
+		log.Fatalln(err)
+	}
+
+	outdata := map[string][]ResultRecord{}
+	for sortimentKategori, apostids := range indata {
+		// log.Printf("%s: %#v\n", sortimentKategori, apostids)
+		for _, apostid := range apostids {
+			reqBody := []byte(fmt.Sprintf(`{"query": {"bool": {"must": [{"match": {"agreements.postIdentifier": "HMDB-%d"}}]}}}`, apostid))
+			resp, err := http.Post("https://hm-grunndata-search.intern.dev.nav.no/products/_search", "application/json", bytes.NewBuffer(reqBody))
+			if err != nil {
+				log.Fatalln(err)
+			}
+			respBody, err := io.ReadAll(resp.Body)
+			resp.Body.Close()
+			if err != nil {
+				log.Fatalln(err)
+			}
+			osResp := OSResponse{}
+			if err = json.Unmarshal(respBody, &osResp); err != nil {
+				log.Fatalln(err)
+			}
+			if _, ok := outdata[sortimentKategori]; !ok {
+				outdata[sortimentKategori] = []ResultRecord{}
+			}
+			if len(osResp.Hits.Hits) > 0 && len(osResp.Hits.Hits[0].Source.Agreements) > 0 {
+				postID := osResp.Hits.Hits[0].Source.Agreements[0].PostID
+				outdata[sortimentKategori] = append(outdata[sortimentKategori], ResultRecord{
+					Apostid: apostid,
+					PostID:  postID,
+				})
+			} else {
+				outdata[sortimentKategori] = append(outdata[sortimentKategori], ResultRecord{
+					Apostid: apostid,
+					PostID:  nil,
+				})
+			}
+			// break
+		}
+		// break
+	}
+
+	outfile, err := json.Marshal(&outdata)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%s\n", outfile)
+}
+
+type OSResponse struct {
+	Hits OSHits `json:"hits"`
+}
+
+type OSHits struct {
+	Hits []OSHit `json:"hits"`
+}
+
+type OSHit struct {
+	Source OSProduct `json:"_source"`
+}
+
+type OSProduct struct {
+	Agreements []OSAgreement `json:"agreements"`
+}
+
+type OSAgreement struct {
+	PostID string `json:"postId"`
+}
+
+type ResultRecord struct {
+	Apostid interface{} `json:"aposid"`
+	PostID  interface{} `json:"postId"`
+}
